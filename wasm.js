5c51a4a750659acfa6a0ac0744f0b57c50bbd236
Tiziano Santoro <tiziano88@gmail.com>
2020-12-11 23:22:10 +0000

    deploy-2020-12-11T23:22:10+00:00

--- wasm.js
+++ wasm.js
@@ -196,20 +196,20 @@ function getInt32Memory0() {
     return cachegetInt32Memory0;
 }
 
-function makeMutClosure(arg0, arg1, dtor, f) {
+function makeClosure(arg0, arg1, dtor, f) {
     const state = { a: arg0, b: arg1, cnt: 1 };
     const real = (...args) => {
         // First up with a closure we increment the internal reference
         // count. This ensures that the Rust closure environment won't
         // be deallocated while we're invoking it.
         state.cnt++;
-        const a = state.a;
-        state.a = 0;
         try {
-            return f(a, state.b, ...args);
+            return f(state.a, state.b, ...args);
         } finally {
-            if (--state.cnt === 0) wasm.__wbindgen_export_2.get(dtor)(a, state.b);
-            else state.a = a;
+            if (--state.cnt === 0) {
+                wasm.__wbindgen_export_2.get(dtor)(state.a, state.b);
+                state.a = 0;
+            }
         }
     };
     real.original = state;
@@ -238,43 +238,14 @@ function logError(f) {
 function _assertNum(n) {
     if (typeof(n) !== 'number') throw new Error('expected a number argument');
 }
-
-let stack_pointer = 32;
-
-function addBorrowedObject(obj) {
-    if (stack_pointer == 1) throw new Error('out of js stack');
-    heap[--stack_pointer] = obj;
-    return stack_pointer;
-}
 function __wbg_adapter_22(arg0, arg1, arg2) {
-    try {
-        _assertNum(arg0);
-        _assertNum(arg1);
-        wasm._dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hec7709b7902f31c6(arg0, arg1, addBorrowedObject(arg2));
-    } finally {
-        heap[stack_pointer++] = undefined;
-    }
+    _assertNum(arg0);
+    _assertNum(arg1);
+    _assertNum(arg2);
+    var ret = wasm._dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h8784f17328c2bf1a(arg0, arg1, arg2);
+    return ret;
 }
 
-function makeClosure(arg0, arg1, dtor, f) {
-    const state = { a: arg0, b: arg1, cnt: 1 };
-    const real = (...args) => {
-        // First up with a closure we increment the internal reference
-        // count. This ensures that the Rust closure environment won't
-        // be deallocated while we're invoking it.
-        state.cnt++;
-        try {
-            return f(state.a, state.b, ...args);
-        } finally {
-            if (--state.cnt === 0) {
-                wasm.__wbindgen_export_2.get(dtor)(state.a, state.b);
-                state.a = 0;
-            }
-        }
-    };
-    real.original = state;
-    return real;
-}
 function __wbg_adapter_25(arg0, arg1, arg2, arg3) {
     _assertNum(arg0);
     _assertNum(arg1);
@@ -283,12 +254,41 @@ function __wbg_adapter_25(arg0, arg1, arg2, arg3) {
     wasm._dyn_core__ops__function__Fn__A_B___Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h89f54eb2745b3d33(arg0, arg1, arg2, arg3);
 }
 
+function makeMutClosure(arg0, arg1, dtor, f) {
+    const state = { a: arg0, b: arg1, cnt: 1 };
+    const real = (...args) => {
+        // First up with a closure we increment the internal reference
+        // count. This ensures that the Rust closure environment won't
+        // be deallocated while we're invoking it.
+        state.cnt++;
+        const a = state.a;
+        state.a = 0;
+        try {
+            return f(a, state.b, ...args);
+        } finally {
+            if (--state.cnt === 0) wasm.__wbindgen_export_2.get(dtor)(a, state.b);
+            else state.a = a;
+        }
+    };
+    real.original = state;
+    return real;
+}
+
+let stack_pointer = 32;
+
+function addBorrowedObject(obj) {
+    if (stack_pointer == 1) throw new Error('out of js stack');
+    heap[--stack_pointer] = obj;
+    return stack_pointer;
+}
 function __wbg_adapter_28(arg0, arg1, arg2) {
-    _assertNum(arg0);
-    _assertNum(arg1);
-    _assertNum(arg2);
-    var ret = wasm._dyn_core__ops__function__Fn__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h8784f17328c2bf1a(arg0, arg1, arg2);
-    return ret;
+    try {
+        _assertNum(arg0);
+        _assertNum(arg1);
+        wasm._dyn_core__ops__function__FnMut___A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hec7709b7902f31c6(arg0, arg1, addBorrowedObject(arg2));
+    } finally {
+        heap[stack_pointer++] = undefined;
+    }
 }
 
 /**
@@ -632,16 +632,16 @@ async function init(input) {
         var ret = wasm.__wbindgen_export_2;
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper2755 = logError(function(arg0, arg1, arg2) {
-        var ret = makeClosure(arg0, arg1, 164, __wbg_adapter_28);
-        return addHeapObject(ret);
-    });
-    imports.wbg.__wbindgen_closure_wrapper4633 = logError(function(arg0, arg1, arg2) {
-        var ret = makeMutClosure(arg0, arg1, 225, __wbg_adapter_22);
-        return addHeapObject(ret);
-    });
-    imports.wbg.__wbindgen_closure_wrapper2757 = logError(function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper2758 = logError(function(arg0, arg1, arg2) {
         var ret = makeClosure(arg0, arg1, 166, __wbg_adapter_25);
+        return addHeapObject(ret);
+    });
+    imports.wbg.__wbindgen_closure_wrapper4634 = logError(function(arg0, arg1, arg2) {
+        var ret = makeMutClosure(arg0, arg1, 225, __wbg_adapter_28);
+        return addHeapObject(ret);
+    });
+    imports.wbg.__wbindgen_closure_wrapper2756 = logError(function(arg0, arg1, arg2) {
+        var ret = makeClosure(arg0, arg1, 164, __wbg_adapter_22);
         return addHeapObject(ret);
     });
 
