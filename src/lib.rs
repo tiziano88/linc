4427fbaa71a5d808115a2736c3253f2aca5b71e9
Tiziano Santoro <tiziano88@gmail.com>
2020-12-21 23:48:53 +0100

    logging

--- src/lib.rs
+++ src/lib.rs
@@ -11,8 +11,11 @@ mod view;
 #[wasm_bindgen(start)]
 pub fn main() {
     // web_logger::init();
+    // wasm_logger::init(wasm_logger::Config::default().module_prefix("some::module"));
+    wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
+
     log::info!("starting");
     // See https://github.com/rustwasm/console_error_panic_hook/issues/8.
     // std::panic::set_hook(Box::new(console_error_panic_hook::hook));
-    App::<Model>::new().mount_to_body();
+    yew::start_app::<Model>();
 }
