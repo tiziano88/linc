2b309d15fec395ac14d9f6653a7290c77bae5835
Tiziano Santoro <tiziano88@gmail.com>
2021-01-01 19:42:09 +0100

    badges

--- src/types.rs
+++ src/types.rs
@@ -295,7 +295,7 @@ impl Component for Model {
             };
             let kinds = parsed_value.kind_hierarchy.iter().map(|k| {
                 html!{
-                    <span class="kind bg-blue-200 p-1 px-2 rounded-md">
+                    <span class="kind bg-blue-200 p-1 px-2 rounded-md text-sm font-mono">
                         <svg class="w-4 h-4 mr-1 inline" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"></path></svg>
                         { k }
                     </span>
@@ -316,7 +316,7 @@ impl Component for Model {
                             { suffix }
                         </span>
                     </div>
-                    <div class="text-sm font-mono">
+                    <div>
                         { for kinds }
                     </div>
                   </div>
