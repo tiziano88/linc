3ffc01e237eaee1b9cddb4a54e8187a6a7c5693d
Tiziano Santoro <tiziano88@gmail.com>
2021-09-24 00:27:16 +0100

    deploy-2021-09-24T00:27:16+01:00

--- wasm.js
+++ wasm.js
@@ -589,7 +589,7 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper4736 = function() { return logError(function (arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper4649 = function() { return logError(function (arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 418, __wbg_adapter_16);
         return addHeapObject(ret);
     }, arguments) };
