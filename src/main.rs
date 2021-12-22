d7f51b81a31d0f281194975310280dacd6bbd06e
Tiziano Santoro <tiziano88@gmail.com>
2021-12-22 20:08:00 +0000

    Convert to binary

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,13 @@
 #![feature(iter_intersperse)]
 
 use types::Model;
-use wasm_bindgen::prelude::*;
 
 mod initial;
 mod schema;
 mod types;
 mod view;
 
-#[wasm_bindgen(start)]
-pub fn main() {
+fn main() {
     // web_logger::init();
     // wasm_logger::init(wasm_logger::Config::default().module_prefix("some::module"));
     wasm_logger::init(wasm_logger::Config::new(log::Level::Trace));
