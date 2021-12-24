4281b8d3c81cc6d4a7a71009be000dd85bed9a9d
Tiziano Santoro <tiziano88@gmail.com>
2021-12-24 00:35:09 +0000

    deploy-2021-12-24T00:35:09+00:00

--- index-1d62d1c5ae902a70.js
+++ index-1d62d1c5ae902a70.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-a9bdb32f66145871_bg.wasm', import.meta.url);
+        input = new URL('index-1d62d1c5ae902a70_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -581,8 +581,8 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper7825 = function(arg0, arg1, arg2) {
-        var ret = makeClosure(arg0, arg1, 386, __wbg_adapter_20);
+    imports.wbg.__wbindgen_closure_wrapper7947 = function(arg0, arg1, arg2) {
+        var ret = makeClosure(arg0, arg1, 400, __wbg_adapter_20);
         return addHeapObject(ret);
     };
 
