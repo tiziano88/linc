0c53623aa894e38393899b3bc8245d2f2857cfd2
Tiziano Santoro <tiziano88@gmail.com>
2021-12-29 14:36:18 +0000

    deploy-2021-12-29T14:36:18+00:00

--- index-ceb35e45c1528b37.js
+++ index-ceb35e45c1528b37.js
@@ -270,7 +270,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-d27a313e33379686_bg.wasm', import.meta.url);
+        input = new URL('index-ceb35e45c1528b37_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -581,7 +581,7 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper9028 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper9000 = function(arg0, arg1, arg2) {
         var ret = makeClosure(arg0, arg1, 469, __wbg_adapter_20);
         return addHeapObject(ret);
     };
