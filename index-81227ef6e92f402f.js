ffb38d71f00b0a4520d486c69caa5de6ce3b88a4
Tiziano Santoro <tiziano88@gmail.com>
2022-01-27 00:50:36 +0000

    deploy-2022-01-27T00:50:36+00:00

--- index-81227ef6e92f402f.js
+++ index-81227ef6e92f402f.js
@@ -314,7 +314,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-df47f567a1395c29_bg.wasm', import.meta.url);
+        input = new URL('index-81227ef6e92f402f_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -680,15 +680,15 @@ async function init(input) {
     imports.wbg.__wbindgen_throw = function(arg0, arg1) {
         throw new Error(getStringFromWasm0(arg0, arg1));
     };
-    imports.wbg.__wbindgen_closure_wrapper4008 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper4007 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 107, __wbg_adapter_24);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper8420 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper8419 = function(arg0, arg1, arg2) {
         var ret = makeClosure(arg0, arg1, 451, __wbg_adapter_27);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper10397 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper10396 = function(arg0, arg1, arg2) {
         var ret = makeMutClosure(arg0, arg1, 470, __wbg_adapter_30);
         return addHeapObject(ret);
     };
