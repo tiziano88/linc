d1fbfb4c6083c844e840bc198b81a472a85ed1cf
Tiziano Santoro <tiziano88@gmail.com>
2022-01-27 23:44:15 +0000

    deploy-2022-01-27T23:44:15+00:00

--- index-f16722be18d51c13.js
+++ index-f16722be18d51c13.js
@@ -314,7 +314,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('index-806f6daeea377856_bg.wasm', import.meta.url);
+        input = new URL('index-f16722be18d51c13_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
