8587f034538095faab31d49d8293238644c68712
Tiziano Santoro <tzn@google.com>
2022-11-19 23:48:23 +0000

    Flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "linc";
+  # https://status.nixos.org/
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+
+  outputs = { self, nixpkgs } : let pkgs = nixpkgs.legacyPackages.x86_64-linux; in {
+    defaultPackage.x86_64-linux =
+      with import nixpkgs { system = "x86-64-linux"; };
+      stdenv.mkDerivation {
+        name = "build";
+        src = self;
+        buildPhase = "echo fo";
+      };
+    devShell.x86_64-linux =
+        pkgs.mkShell { 
+          buildInputs = [
+            pkgs.go
+            pkgs.gopls
+
+            pkgs.docker
+            pkgs.python3
+          ]; 
+        };
+    };
+}
