20b8ecf346304c50957e47ae4167df2fb13a4925
Tiziano Santoro <tzn@google.com>
2022-11-20 00:54:36 +0000

    Fix flake

--- flake.nix
+++ flake.nix
@@ -1,22 +1,35 @@
 {
   description = "linc";
-  # https://status.nixos.org/
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+  inputs= {
+    # https://status.nixos.org/
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-22.05;
+    rust-overlay.url = github:oxalica/rust-overlay;
+  };
 
-  outputs = { self, nixpkgs } : let pkgs = nixpkgs.legacyPackages.x86_64-linux; in {
-    defaultPackage.x86_64-linux =
-      with import nixpkgs { system = "x86-64-linux"; };
-      stdenv.mkDerivation {
-        name = "build";
-        src = self;
-        buildPhase = "echo fo";
+  outputs = { self, nixpkgs, rust-overlay } :
+    let
+      pkgs = import nixpkgs {
+        overlays = [ rust-overlay.overlays.default ];
+        system = "x86_64-linux";
       };
-    devShell.x86_64-linux =
-        pkgs.mkShell { 
-          buildInputs = [
-            pkgs.cargo
-            pkgs.protobuf
-          ]; 
-        };
+    in {
+        defaultPackage.x86_64-linux =
+            pkgs.stdenv.mkDerivation {
+                name = "build";
+                src = self;
+                buildPhase = "echo fo";
+            };
+        devShell.x86_64-linux =
+            pkgs.mkShell {
+                buildInputs = [
+                    pkgs.cargo
+                    pkgs.protobuf
+                    pkgs.trunk
+                    (pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+                        extensions = [ "rust-src" ];
+                        targets = [ "wasm32-unknown-unknown" ];
+                    }))
+                ];
+            };
     };
 }
